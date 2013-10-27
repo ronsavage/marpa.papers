50a32b0b0ab8bbd0a3a37aa504a43c026fcf7026
ronsavage <ron@savage.net.au>
2013-10-27 13:22:29 +1100

    Move build/ to scripts/. Add scripts/build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+perl scripts/guide.pl
+
+DIR=Perl-modules/html/marpa.papers
+export DIR
+
+mkdir -p $DR/$DIR
+cp out/* $DR/$DIR
+
+mkdir -p ~/savage.net.au/$DIR
+cp out/* ~/savage.net.au/$DIR
+
+echo Copied HTML to ~/savage.net.au/... and $DR/$DIR
